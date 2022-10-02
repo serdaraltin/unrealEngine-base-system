59a792419011f321677ee92f2cb77060604f8ec5
serdaraltin <meyta.tr@gmail.com>
2022-10-02 22:11:56 +0300

    Revert "Add the Base Third Person movement system"
    This reverts commit 44359b4433960e8980a05988abf00fe79dd3892f.

--- Source/Base_System/Function_Library/BS_Library.cpp
+++ Source/Base_System/Function_Library/BS_Library.cpp
@@ -5,7 +5,6 @@
 #include "DrawDebugHelpers.h"
 #include "VectorTypes.h"
 #include "Math/Color.h"
-#include "Util/ColorConstants.h"
 
 // Sets default values
 ABS_Library::ABS_Library()
@@ -29,12 +28,12 @@ void ABS_Library::Tick(float DeltaTime)
 FHitResult ABS_Library::MakeSingleLineTrace(
 	AActor* AIgnoreActor, float FDistance, 
 	bool bDrawDebugLine, FLinearColor DebugColor,
-	bool bPrintHitActorName, FLinearColor NameColor, bool bRandomColor)
+	bool bPrintHitActorName, FLinearColor NameColor)
 {
-	float r = random()+1.f, g=random()+1.f, b=random()+1.f;
+
 	UWorld *UWorld = GEngine->GameViewport->GetWorld();
 	FVector FStartPoint = AIgnoreActor->GetActorLocation();
-	FVector FEndPoint = (AIgnoreActor->GetActorUpVector() * (-1*FDistance)) + FStartPoint;
+	FVector FEndPoint = (AIgnoreActor->GetActorForwardVector() * FDistance) + FStartPoint;
 	FHitResult FHitResult;
 	FCollisionQueryParams QueryParams;
 	QueryParams.AddIgnoredActor(AIgnoreActor);
@@ -42,8 +41,7 @@ FHitResult ABS_Library::MakeSingleLineTrace(
 		ECollisionChannel::ECC_Camera, QueryParams);
 	if (bDrawDebugLine)
 		DrawDebugLine(UWorld, FStartPoint, FEndPoint,
-			(bRandomColor) ? DebugColor.ToFColor(false): UE::Geometry::LinearColors::MakeColor3f<FColor>(r,g,b),
-			false, 5.f,0,1.5f);
+			DebugColor.ToFColor(false), false, 2.f);
 	if (bPrintHitActorName && FHitResult.GetActor())
 		GEngine->AddOnScreenDebugMessage(
 			1, 1.f, NameColor.ToFColor(false),
