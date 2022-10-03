4f06e6bc48be33fac0838a0f0a6756926beb3a46
serdaraltin <meyta.tr@gmail.com>
2022-10-03 16:36:43 +0300

    update

--- Source/Base_System/Base_SystemGameModeBase.h
+++ Source/Base_System/Base_SystemGameModeBase.h
@@ -6,14 +6,6 @@
 #include "GameFramework/GameModeBase.h"
 #include "Base_SystemGameModeBase.generated.h"
 
-<<<<<<< HEAD:Source/Base_System/Character/BS_ThirdPersonGameMode.h
-UCLASS(minimalapi)
-class ABS_ThirdPersonGameMode : public AGameModeBase
-{
-	GENERATED_BODY()
-public:
-	ABS_ThirdPersonGameMode();
-=======
 /**
  * 
  */
@@ -21,6 +13,5 @@ UCLASS()
 class BASE_SYSTEM_API ABase_SystemGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
->>>>>>> parent of 44359b4 (Add the Base Third Person movement system):Source/Base_System/Base_SystemGameModeBase.h
 	
 };
